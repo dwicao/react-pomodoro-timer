759e878c1c65c2cc393c14e7f928353c4444186d
dwicao <lutfiandwica@gmail.com>
2016-10-18 09:32:59 +0700

    added css loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,15 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader'}
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+		        test: /\.css$/,
+		        loader: 'style-loader!css-loader' 
+		    }
 		]
 	},
 	plugins: [HtmlWebpackPluginConfig]
