a4bf1d371bed9b59e10ad84d18019d791ea252b2
dwicao <lutfiandwica@gmail.com>
2016-11-03 08:38:23 +0700

    fix routes for page not found

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import Main from '../components/Main';
 import Home from '../components/Home';
-import NotFound from '../components/NotFound';
 
 
 
@@ -11,7 +10,6 @@ const routes = (
 		<Route path='/' component={Main}>
 			<IndexRoute component={Home} />
 		</Route>
-		<Route path='*' component={NotFound} />
 	</Router>
 );
 
